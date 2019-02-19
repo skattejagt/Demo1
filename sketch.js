89a12828b08a3cbcbed641b23d0e8f74499eec50
Esbern Holmes <holmes@ruc.dk>
2019-02-19 22:54:37 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,8 @@
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
-function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+function draw(){
+  background(255);
+  box();
 }
